0e8e1df42fac18217e4caeaef4e98adc3f6f2d6b
Lelabeshidze <lela.beshidze@gmail.com>
2022-08-04 18:12:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,45 @@ let countString = stringArray.reduce((count, currentValue) => {
 console.log(countString);
 
 // thirdtask
+class Car {
+    constructor(brand,model){
+        this.brand = brand ;
+        this.model = model ;
+        this.speed = 0;
+        this.motion = "The car is not moving";
+    }
+    check_motion(speed) {
+        this.speed = speed ;
+        if (speed <=0){
+            console.log(this.motion = "The car is stopped");
+        } else {console.log(this.motion = "The car is moving")}
+    }
+    accelerate(speed) {
+        this.speed += 25;
+        console.log(this.speed);
+    }
+    brake (speed){
+        this.speed -=20;
+        if(this.speed<0) {this.speed = 0;}
+        console.log (this.speed);
+    }
+    emergency_brake () {
+        this.speed = 0;
+        console.log(this.speed);
+    };
+    status () {
+        this.check_motion();
+        this.speed = 0 ;
+        console.log(`${this.brand} ${this.model} is moving at ${this.speed} km/h and the status is: ${this.motion}`);
+    }
+}
+
+let car = new Car("Toyota", "Camry");
+car.check_motion(20);
+car.accelerate();
+car.brake();
+car.accelerate();
+car.emergency_brake();
+car.status();
+car.status();
+
